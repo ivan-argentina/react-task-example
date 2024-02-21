163cb0c3e8e3a657cf9f5ef7414bbe02dc0a3a0a
Ivan Genari <sistemaamedida19@gmail.com>
2024-02-21 13:50:57 -0300

    first

---
+++ src/Data/tasks.js
@@ -0,0 +1,21 @@
+export const tasks = [
+    {
+        id:0,
+        title: "Mi Primer Tarea",
+        description: 'Mi Primer Tarea',
+
+    },
+    {
+        id:1,
+        title: "Mi Segunda Tarea",
+        description: 'Mi Segunda Tarea',
+
+    },
+    {
+        id:2,
+        title: "Mi Tercer Tarea",
+        description: 'Mi Tercer Tarea',
+
+    },
+
+];
